cb9441b366342d65a9decddd03d37d29d547389b
Monica Nickolai <monica.nickolai@gmail.com>
2016-03-10 13:42:05 +0900

    limited user abilities to edit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
-
+  load_and_authorize_resource
   # GET /products
   # GET /products.json
   def index
@@ -10,8 +10,7 @@ class ProductsController < ApplicationController
   # GET /products/1
   # GET /products/1.json
   def show
-    @comments = @product.comments.order("created_at DESC")
-    @comments = @product.comments.paginate(:page => params[:page], :per_page => 2)
+    @comments = @product.comments.order("created_at DESC").paginate(:page => params[:page], :per_page => 2)
   end
 
   # GET /products/new
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,9 +4,12 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     	if user.admin?
-    		can :manage, all
+    		can :manage, :all
     	else
     can :manage, User, id: user.id
+    can :manage, Product, id: user.id 
+    can :read, :all
+    can :create, Product
   end
 end
 end
