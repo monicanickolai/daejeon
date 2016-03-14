41e8ed638b962094224521870a1abfaaaeb9e89f
Monica Nickolai <monica.nickolai@gmail.com>
2016-03-14 18:24:11 +0900

    changed abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,12 +5,9 @@ class Ability
     user ||= User.new # guest user (not logged in)
     	if user.admin?
     		can :manage, :all
-        can :manage, User
-        can :manage, Comment
-    	else
-    can :manage, Product 
+    	else 
     can :manage, User, id: user.id
-    can [:edit, :update, :destroy], Product, user_id: user.id
+    can :create, User
     can :destroy, Comment, user_id: user.id
     can :read, :all
   end
---
+++ spec/models/product_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe Product do
+context "when the product has comments" do
+	before do
+	@product = Product.create!(name: "painting")
+	@user = User.create!(email: "artist@gmail.com", password: "painting")
+	@product.comments.create!(rating: 1, user: @user, body: "Awful!")
+	@product.comments.create!(rating: 3, user: @user, body: "OK")
+	@product.comments.create!(rating: 5, user: @user, body: "Great!")
+	end
+	
+	it "returns the average rating of all comments" do
+		expect(@product.average_rating).to eq 3.0
+	end
+
+
+context "Product has no name" do
+	before do
+	@product = Product.new(description: "Nice bike")
+end
+
+	it "is invalid" do
+	expect(Product.new(description: "Nice bike")).not_to be_valid
+
+end
+end
+end
+end
