68caa0530de32d138ca50cb6298107f639746d43
Monica Nickolai <monica.nickolai@gmail.com>
2016-03-16 20:45:08 +0900

    edited spec file names

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,20 +1,19 @@
 require 'rails_helper'
 
 describe Product do
-context "when the product has comments" do
-	before do
-		@product = Product.create!(name: "painting")
-		@user = User.create!(email: "artist@gmail.com", password: "painting")
-		@product.comments.create!(rating: 1, user: @user, body: "Awful!")
-		@product.comments.create!(rating: 3, user: @user, body: "OK")
-		@product.comments.create!(rating: 5, user: @user, body: "Great!")
-	end
-	
-	it "returns the average rating of all comments" do
-		expect(@product.average_rating).to eq 3.0
+
+	context "Product has no name" do
+		before do
+		@product = Product.new(description: "Nice bike")
 	end
 
+	it "is valid" do
+		expect(Product.new(name: "Huffy", description: "Nice bike")).to be_valid
+	end
 
+	it "is invalid" do
+		expect(Product.new(description: "Nice bike")).not_to be_valid
 
+	end
 end
 end
