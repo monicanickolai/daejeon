234ccde3fd1bdaa229837e1908c8aa0603281341
Monica Nickolai <monica.nickolai@gmail.com>
2016-04-10 17:17:10 +0900

    fix Stripe API

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -7,7 +7,7 @@ else
   Rails.configuration.stripe = {
     :publishable_key => 'pk_test_hzaM4OSb2klo5vFrfnk8Yxo6',
     :secret_key => 'sk_test_Uggz7bSLZ0Sh8fSll55Cy1MQ'
-  }
+}
 end
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
