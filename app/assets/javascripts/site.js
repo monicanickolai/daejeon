06fc4063c0c3cf5d3df815e7438f5ddaf44351e3
Monica Nickolai <monica.nickolai@gmail.com>
2016-04-17 16:34:59 +0900

    added elevate zoom

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,4 +1,8 @@
 $(document).on('ready page:load', function(){
+	//JQuery elevate zoom
+	$('.img-zoom').elevateZoom();
+
+	//star rating system
     $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
     $('.rated').raty({ path: '/assets',
       readOnly: true,
